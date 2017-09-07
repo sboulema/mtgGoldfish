4c82da055fa4d6fc199b299477f096db4dd98a5b
Samir L. Boulema <sboulema@gmail.com>
2017-09-07 17:09:27 +0200

    Custom tokens

--- js/token.js
+++ js/token.js
@@ -13,10 +13,48 @@ function setupTokens() {
     addTokensToSelect();
 
     $('#token-select').on('change', function() {
-        createCard({
-            multiverseId: this.value,
-            layout: "token"
-        }).appendTo("#table");
-        bindCardActions();
+        if (this.value === "0") {
+            $('#tokenModal').modal('show');
+        } else {
+            createCard({
+                multiverseId: this.value,
+                layout: "token"
+            }).appendTo("#table");
+            bindCardActions();
+        }     
     })
+}
+
+function createToken(name, rules, powerToughness) {
+    var token = createCard({multiverseId: 0, layout: "token"});
+    token.children(".front")[0].style.backgroundImage = "url('img/token.png')";
+
+    $('<div/>')
+    .addClass("mtg-card-token-name")
+    .html(name)
+    .appendTo(token.children(".front"));
+
+    $('<div/>')
+    .addClass("mtg-card-token-type")
+    .html(name)
+    .appendTo(token.children(".front"));
+
+    $('<div/>')
+    .addClass("mtg-card-token-rules")
+    .html(rules)
+    .appendTo(token.children(".front"));
+
+    $('<div/>')
+    .addClass("mtg-card-token-powerToughness")
+    .html(powerToughness)
+    .appendTo(token.children(".front"));
+
+    return token;
+}
+
+function addCustomToken() {
+    createToken($("#token-name").val(), $("#token-rules").val(), $("#token-powerToughness").val())
+        .appendTo("#table");
+    $('#tokenModal').modal('hide');
+    bindCardActions();
 }
